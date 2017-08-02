1fa8c84884c29bf5c5421a609221e27a3f23c77f
erwang01 <erwang01@gmail.com>
2017-08-02 12:40:51 -0700

    Update gpio.py to pep8

--- gpio.py
+++ gpio.py
@@ -5,16 +5,17 @@ import constants
 def washer():
     try:
         relayWasher = DigitalOutputDevice(constants.WASHER)
-        relayWasher.blink(constants.CLOSE_TIME,constants.OPEN_TIME,1,False)
+        relayWasher.blink(constants.CLOSE_TIME, constants.OPEN_TIME, 1, False)
         relayWasher.close()
     except GPIOZeroError:
         return 1
     return 0
 
+
 def dryer():
     try:
         relayDryer = DigitalOutputDevice(constants.DRYER)
-        relayDryer.blink(constants.CLOSE_TIME,constants.OPEN_TIME,1,False)
+        relayDryer.blink(constants.CLOSE_TIME, constants.OPEN_TIME, 1, False)
         relayWasher.close()
     except GPIOZeroError:
         return 1

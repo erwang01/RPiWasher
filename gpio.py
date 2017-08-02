0e8a58c9b4ffbdc2e5d1509adee205c9fd88e9db
Eric Wang <erwang01@gmail.com>
2017-08-02 12:33:40 -0700

    basic relay functionality

---
+++ gpio.py
@@ -0,0 +1,21 @@
+from gpiozero import DigitalOutputDevice
+import constants
+
+
+def washer():
+    try:
+        relayWasher = DigitalOutputDevice(constants.WASHER)
+        relayWasher.blink(constants.CLOSE_TIME,constants.OPEN_TIME,1,False)
+        relayWasher.close()
+    except GPIOZeroError:
+        return 1
+    return 0
+
+def dryer():
+    try:
+        relayDryer = DigitalOutputDevice(constants.DRYER)
+        relayDryer.blink(constants.CLOSE_TIME,constants.OPEN_TIME,1,False)
+        relayWasher.close()
+    except GPIOZeroError:
+        return 1
+    return 0
